558901aa52b06f5b93d4a3233d184a2ec1fa861c
bodiavaleur <bodiavaleur@gmail.com>
2019-01-27 11:13:22 +0200

    Updates

---
+++ precache-manifest.bbecd9d5e183a00b5a6732bb7bf7d94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e606ba8ddd7d702a1fb",
+    "url": "/shoplistapp/static/js/runtime~main.1e606ba8.js"
+  },
+  {
+    "revision": "c057a3a8e87620e17d5d",
+    "url": "/shoplistapp/static/js/main.c057a3a8.chunk.js"
+  },
+  {
+    "revision": "7c3aec79f3ae62290828",
+    "url": "/shoplistapp/static/js/1.7c3aec79.chunk.js"
+  },
+  {
+    "revision": "c057a3a8e87620e17d5d",
+    "url": "/shoplistapp/static/css/main.4f476dbc.chunk.css"
+  },
+  {
+    "revision": "06261bb551e58bff8d9dcd0fe8f76e86",
+    "url": "/shoplistapp/index.html"
+  }
+];
